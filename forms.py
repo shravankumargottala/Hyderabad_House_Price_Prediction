2b11a115601c6c2f0af5552dac5ffc965eda063e
shravankumargottala <48157465+shravankumargottala@users.noreply.github.com>
2019-11-19 19:14:22 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -136,6 +136,6 @@ class FlatPrice(FlaskForm):
     Furn_status = SelectField('Furnishing Status:', choices=tuple(furn_main_list))
     Fac_status = SelectField('Facing:', choices=tuple(fac_main_list))
     Addrss = SelectField('Address:', choices=tuple(addr_main_list))
-    submit = SubmitField('Predict', render_kw={"onclick": "checkform()"},default={})
+    submit = SubmitField('Predict', render_kw={"onclick": "checkform()"},default=0)
     
     
