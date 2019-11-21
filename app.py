70b88cc4345ebe4269f95d15d3b9d433ca331be2
shravankumargottala <48157465+shravankumargottala@users.noreply.github.com>
2019-11-20 21:00:32 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -122,32 +122,30 @@ def flat_predict():
     for l in range(len(Flat_addr_Uniq)):
         flat_addr_dict[Flat_addr_Uniq[l]] = l
     print("flat_addr_dict is ",flat_addr_dict)
-
+  
     if request.method == 'POST':
-        no_of_bed = request.form['No_of_Bedrooms']
-        no_of_bath = request.form['No_of_Bathrooms']
-        no_of_bolc = request.form['No_of_Balconies']
-        no_of_pooja = request.form['No_of_Poojarooms']
-        sup_area = request.form['Supr_Area']
-        car_area = request.form['Carp_Area']
-        flr_no = request.form['Floor_No']
-        print("floor number is ",flr_no)
-        total_flrs = request.form['Total_Floors']
-        print("total_flrs number is ",total_flrs)
-        furn_status = request.form['Furn_status']
-        print("furn_status number is ",furn_status)
-        house_facing = request.form['Fac_status']
-        print("house_facing is ",house_facing)
-        house_addres = request.form['Addrss']
-        print("house_addres is ",house_addres,flat_addr_dict[house_addres])
-        int_features =[no_of_bed,no_of_bath,no_of_bolc,no_of_pooja,sup_area,car_area,flr_no,total_flrs,flat_furn_stat_dict[furn_status],flat_fac_dict[house_facing],flat_addr_dict[house_addres]]
-        print("int_features is ",int_features)
-        final_features = [np.array(int_features)]
-        prediction = model.predict(final_features)
-        output = round(np.expm1(prediction[0]),2)
+        if form.validate() == False :
+            flash("All fields are manadatory")
+        else:
+                
+            no_of_bed = request.form['No_of_Bedrooms']
+            no_of_bath = request.form['No_of_Bathrooms']
+            no_of_bolc = request.form['No_of_Balconies']
+            no_of_pooja = request.form['No_of_Poojarooms']
+            sup_area = request.form['Supr_Area']
+            car_area = request.form['Carp_Area']
+            flr_no = request.form['Floor_No']
+            total_flrs = request.form['Total_Floors']
+            furn_status = request.form['Furn_status']
+            house_facing = request.form['Fac_status']
+            house_addres = request.form['Addrss']
+            int_features =[no_of_bed,no_of_bath,no_of_bolc,no_of_pooja,sup_area,car_area,flr_no,total_flrs,flat_furn_stat_dict[furn_status],flat_fac_dict[house_facing],flat_addr_dict[house_addres]]
+            final_features = [np.array(int_features)]
+            prediction = model.predict(final_features)
+            output = round(np.expm1(prediction[0]),2)
 
-        return render_template('flat_result.html', prediction_text='{} House Price for the below Specifications will be ₹ {}/- Lacs'.format(house_addres,output),no_of_bed_rooms='Flat type : {} BHK '.format(no_of_bed),sup_area='Super Area : {} sqft'.format(sup_area),car_area='Carpet Area : {} sqft'.format(car_area),house_facing='House Facing : {}'.format(house_facing),furn_status='Furnishing status: {}.'.format(furn_status),house_addres='House Address : {}, Hyderabad'.format(house_addres))
-    
+            return render_template('flat_result.html', prediction_text='{} House Price for the below Specifications will be ₹ {}/- Lacs'.format(house_addres,output),no_of_bed_rooms='Flat type : {} BHK '.format(no_of_bed),sup_area='Super Area : {} sqft'.format(sup_area),car_area='Carpet Area : {} sqft'.format(car_area),house_facing='House Facing : {}'.format(house_facing),furn_status='Furnishing status: {}.'.format(furn_status),house_addres='House Address : {}, Hyderabad'.format(house_addres))
+        
         
 if __name__ == '__main__':
     app.run(debug=True)
